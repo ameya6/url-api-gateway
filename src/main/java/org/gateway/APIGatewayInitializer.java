edafdaf9d483d3de85acc9a0094819a5b3cd35eb
Ameya <ameyasawant5@gmail.com>
2024-09-25 17:04:09 +0530

    Initial commit

---
+++ src/main/java/org/gateway/APIGatewayInitializer.java
@@ -0,0 +1,16 @@
+package org.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class APIGatewayInitializer {
+    public static void main(String[] args) throws UnknownHostException {
+        System.setProperty("hostName", InetAddress.getLocalHost().getHostName());
+        System.setProperty("hostAddress", InetAddress.getLocalHost().getHostAddress());
+        SpringApplication.run(APIGatewayInitializer.class, args);
+    }
+}
---
+++ src/main/java/org/gateway/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package org.gateway;
+
+import io.netty.handler.logging.LogLevel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
+
+@Configuration
+public class ApplicationConfig {
+
+    /*@Bean
+    HttpClient httpClient() {
+        return HttpClient.create().wiretap("LoggingFilter", LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL);
+    }*/
+}
